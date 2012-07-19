2fa4483cf307b16827a8a8dcf381b37a0068db90
Derek <derek_rockwell@uhc.com>
2012-07-19 14:37:49 -0500

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdfitRails::Application.config.secret_token = 'afe6a265110c43e11e70a16f774cfb8fc61843f21746190f68afd43495d8acc1d15b5d2f0c1ae4e9cea97c77eda8e6e3f09c0abac2e80d913372b176e41946e0'
